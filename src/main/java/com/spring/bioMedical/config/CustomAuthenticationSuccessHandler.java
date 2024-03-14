915612ee1fdcc99f3183f6c2b660419747ab2925
Claudius Joel <92925712+joelclaudius@users.noreply.github.com>
2024-03-15 00:00:03 +0300

    Update CustomAuthenticationSuccessHandler.java

--- src/main/java/com/spring/bioMedical/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/spring/bioMedical/config/CustomAuthenticationSuccessHandler.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
